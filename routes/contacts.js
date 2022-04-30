9e20249b8c9c1bc60badf31f7834be2d9176aea9
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2022-04-30 17:17:03 -0600

    Add REST endpoints

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,13 +1,33 @@
 const routes = require('express').Router();
 const connect = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId; // This is so we can query by the db ID
 
 routes.get('/', (req, res) => {
     // Get the collection of data from the db
-    connect.getCollection().find().toArray(function(err, result) {
-        if (err) throw err;
-        res.json(result);
-        console.log("Contacts have been retrieved");
+    const results = connect.getCollection().find()
+    
+    // Make the results into an array
+    results.toArray().then((documents) => {
+        res.status(200).json(documents);
+        console.log("Returned all contacts");
     });
+    
+});
+
+routes.get('/:id', (req, res) => {
+
+    // Get the ID from the request
+    const contactId = new ObjectId(req.params.id);
+
+    // Get the collection of data from the db that matches the contactId
+    const results = connect.getCollection().find({_id: contactId})
+    
+    // Make the results into an array
+    results.toArray().then((documents) => {
+        res.status(200).json(documents[0]); // Grab a single object
+        console.log(`Returned Contact ${req.params.id}`);
+    });
+    
 });
 
 module.exports = routes;
